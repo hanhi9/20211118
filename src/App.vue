1dc69086dcd3321bc54cb70f8417c7edca20299c
hanhi7 <hanhi7@softlab-hum.dev>
2021-11-25 22:27:13 +0900

    v-model 사용 ,mounted , computed , unmounted  사용후 이해

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,19 @@
 
     <div class="input-group input-group-sm mb-3">
   <span class="input-group-text" id="inputGroup-sizing-sm">E-mail</span>
-  <input type="text" class="form-control" > 
+  <input type="text" v-model="inpEmail02" class="form-control" > 
 </div>
 <div class="input-group input-group-sm mb-3">
   <span class="input-group-text" id="inputGroup-sizing-name">이름</span>
-  <input type="text" class="form-control" > 
+  <input type="text" v-model="name" class="form-control" > 
 </div>
 <div class="input-group input-group-sm mb-3">
   <span class="input-group-text" id="inputGroup-sizing-age">나이</span>
-  <input type="text" class="form-control" > 
+  <input type="text" v-model="age" class="form-control" > 
 </div>
 <div class="input-group input-group-sm mb-3">
-  <span class="input-group-text" id="inputGroup-sizing-size">사이즈</span>
-  <input type="text" class="form-control" > <b-button variant="success">Button</b-button>
+  <span class="input-group-text" id="inputGroup-sizing-size">비번</span>
+  <input type="text" v-model="pw" class="form-control" > <b-button variant="success">Button</b-button>
 </div>
 <div class="btn-group" role="group" aria-label="Basic example">
   <button type="button" class="btn btn-primary">Left</button>
@@ -46,9 +46,9 @@
   <input type="password" v-model="inPsw" class="form-control" id="floatingPassword" placeholder="Password">
   <label for="floatingPassword">Password</label>
 </div>
+<div > {{message}} </div>
 
 <b-button variant="success" @click="getData">Button</b-button>
-
   </div>
   
  
@@ -56,39 +56,45 @@
 </template>
 
 <script>
-// export default {
-//   name: 'app',
-//   methods:{
-//     showAlert(){
-//       alert("han@nate.com \n 1234")
-//     }
-//   }
-
-// }
 export default {
   name: 'App',
   data () {
     return {
-      inpEmail: 'email',
-      inPsw: 'password'
+      inpEmail: '',
+      inPsw: '',
+      inpEmail02: '',
+      name: '',
+      age: '',
+      pw: '',
+      aa:'속성',
+      uri:'www.naver.com'
     }
   },
   methods: {
     getData () {
-      alert('이메일 : ' + this.inpEmail + '\n password : ' + this.inPsw)
+      //alert('이메일 : ' + this.inpEmail + '\n password : ' + this.inPsw)
+      var conf = confirm("로그인 하시겠습니까?")
+      if(conf){
+          this.inpEmail02 =  this.inpEmail  // 이메일
+          this.name =  'hanhi7'             // 이름
+          this.age =  42                    // 나이
+          this.pw =  this.inPsw             // 비번
+      }
     }
+  },
+  mounted(){
+    console.log("화면 onload");
+  },
+  // unmounted(){
+  //   console.log("11111");
+  // },
+  computed:{
+    message(){
+      console.log("반환하는 데이터를 기억해 두어 그 데이터를 다시 반환");
+    }
+    
   }
-  // methods: { 
-  //   fnGetVal () {
-  //     var valName = document.getElementById('nm').value
-  //     alert(valName)
-  //   }
-  // }
 }
-// function fnGetVal () { // eslint-disable-line no-unused-vars
-//   var valName = document.getElementById('nm').value
-//   alert(valName)
-// }
 </script>
 
 <style>
