b7f6c65be76ecb3080cd36af097c6c06d09cd1b9
hanhi7 <hanhi7@softlab-hum.dev>
2021-11-25 23:12:56 +0900

    router 설치 파일 및 분리 파일 신규등록

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import about from '@/views/about.vue';
+import home from '@/views/home.vue';
+import login from '@/views/login.vue';
+import main from '@/views/main.vue';
 
 Vue.use(Router)
 
@@ -10,6 +14,26 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'about',
+      component: about
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/',
+      name: 'main',
+      component: main
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: login
     }
   ]
 })
